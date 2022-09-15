b676bcdd09eeb23a04d5e8565605a96a69509ea7
lawrencegraham <lgraham274@yahoo.com>
2022-09-15 05:03:20 -0400

    Delete functionality implemented

--- index.js
+++ index.js
@@ -28,8 +28,44 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-//Listen for connections 
+let placesFormatted = data.places.map((place, index) => {
+    return (
+      <div className="col-sm-6">
+        <h2>
+          <a href={`/places/${index}`} >
+            {place.name}
+          </a>
+        </h2>
+        <p>className="text-center" 
+          {place.cuisines}
+        </p>
+        <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    )
+  })
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+  
+  
+// Listen for Connections
 app.listen(process.env.PORT)
+//Listen for connections 
+
 
 
 // res.status(404).render('error404')
